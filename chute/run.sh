56316ba1708bff6e9f3548c05ce42d7007dfaa4d
Jiaan <jwang682@wisc.edu>
2016-12-10 23:15:51 -0600

    111

--- chute/run.sh
+++ chute/run.sh
@@ -4,6 +4,4 @@ chmod 777 /music
 iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o eth0 -j MASQUERADE
 /etc/init.d/apache2 restart
 /usr/bin/rygel
-chmod -R 777 /root/.cache/rygel
-chmod 777 /root/.cache
-chmod 777 /root
+chmod -R 777 /root
