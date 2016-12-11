dd6c9bb580ac7a1a27a94ffb1f93e4ce230d3a25
Jiaan <jwang682@wisc.edu>
2016-12-10 22:40:40 -0600

    some bugs about permisson

--- chute/run.sh
+++ chute/run.sh
@@ -3,8 +3,7 @@
 chmod 777 /music
 chmod 777 /root
 chmod 777 /root/.cache
-chmod 777 /root/.cache/rygel
-chmod 777 /root/.cache/rygel/media-export.db
+chmod -R 777 /root/.cache/rygel
 iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o eth0 -j MASQUERADE
 /etc/init.d/apache2 restart
 /usr/bin/rygel
