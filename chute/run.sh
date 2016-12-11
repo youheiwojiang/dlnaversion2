1adfc9cecd2c90ac744920168f2be179a5884952
bjw <bjw@bjw-Lenovo-IdeaPad-Y500.(none)>
2016-12-11 10:08:22 -0600

    umask

--- chute/run.sh
+++ chute/run.sh
@@ -5,7 +5,7 @@ chmod 777 /root
 chmod 777 /root/.cache
 chmod 777 /root/.cache/rygel
 chmod 777 /root/.cache/rygel/*
-
+umask 000
 iptables -t nat -A POSTROUTING -s 192.168.0.0/16 -o eth0 -j MASQUERADE
 /etc/init.d/apache2 restart
 /usr/bin/rygel
